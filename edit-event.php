d42c27da13503b2a969ce9ee451c7b1582815c9e
Tamunodokubo Whyte <whytetj@cardiff.ac.uk>
2019-03-24 22:24:19 +0000

    Update edit-event.php

--- edit-event.php
+++ edit-event.php
@@ -8,5 +8,5 @@ $end = $_POST['end'];
 
 $sqlUpdate = "UPDATE tbl_events SET title='" . $title . "',start='" . $start . "',end='" . $end . "' WHERE id=" . $id;
 mysqli_query($conn, $sqlUpdate)
-mysqli_close($conn);
+//mysqli_close($conn);
 ?>
