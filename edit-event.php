b9b8ec2568172f762a68e7620ae813678a3ba6c9
Tamunodokubo Whyte <whytetj@cardiff.ac.uk>
2019-06-03 03:42:27 +0100

    Update edit-event.php
    The initial commit was buggy

--- edit-event.php
+++ edit-event.php
@@ -1,25 +1,17 @@
 <?php
+include "dbconnect.php";
 
-  if(isset($_POST['submitted'])){ // checks if it actually submits  and if it does does the following below
-      include"dbconnect.php";
-      $title = $_POST['title'];
-      $start = $_POST['start'].' '.$_POST['starttime'].':00';
-      $titlenew = $_POST['titlenew'];
-      $startnew = $_POST['startnew'].' '.$_POST['starttimenew'].':00';
-      $endnew = $_POST['endnew'].' '.$_POST['endtimenew'].':00';
-      
-      $sqlUpdate = "UPDATE events_table SET title='$titlenew', start='$startnew', end='$endnew' WHERE title='$title' AND start='$start'";
+if(isset($_POST['title'])){
 
-      if(!mysqli_query($conn,$sqlUpdate)){ //nested if statement if there is issue adding to the database
+$title = $_POST['title'];
+$start = $_POST['start'];
+$end = $_POST['end'];
 
-        die("Error editing the data");
+$sqlUpdate = "UPDATE events_table SET start='" . $start . "',end='" . $end . "' WHERE title= '$title' ";
 
-      } else{
-        //$success = "Entry has been successfully entered"; //PHP variable will be echoed in the html form
-        echo "Entry has been edited";
-      }
+$conn->query($sqlUpdate);
+}
 
-  }
-  
+$conn->close();
 
 ?>
